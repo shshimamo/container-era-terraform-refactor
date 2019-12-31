f28079f35f10821703903a452d7a44d83b524cf3
hiroya.shimamoto <shshimamo@gmail.com>
2020-01-01 02:34:48 +0900

    Add aws_iam_policy, and attachment for ECS Task
    4.5 ロードバランサの作成 まで終了
    serviceディレクトリでterraform applyを実行したら
    ECSのタスクがRUNNING状態になることを確認。
    ALBのDNSにアクセスするとHello world!が表示されることを確認

--- iam/aws_iam_policy.tf
+++ iam/aws_iam_policy.tf
@@ -2,6 +2,10 @@
 # path:        一意なパス
 # description: ポリシーの説明
 # policy:      ポリシーの詳細。今回はfile関数でjsonを指定
+
+# 権限はポリシーで定義する
+# ポリシーはポリシードキュメントというjsonで記述する
+# ポリシードキュメントを保持するリソースがIAMポリシー(aws_iam_policy)
 resource "aws_iam_policy" "deploy" {
   name = "deploy"
   path = "/"
@@ -15,4 +19,11 @@ resource "aws_iam_policy" "ecs_instance_policy" {
   path = "/"
   description = ""
   policy = "${file("aws_iam_policies/ecs_instance_policy.json")}"
+}
+
+resource "aws_iam_policy" "ecs_task_policy" {
+  name        = "ecs-task-policy"
+  path        = "/"
+  description = ""
+  policy      = file("aws_iam_policies/ecs_task_policy.json")
 }
